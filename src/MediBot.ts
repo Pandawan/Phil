e91460fddd5fdb6aa1aced7fdd79e58930d1a2e5
KhoomeiK <rohan.pandey@gmail.com>
2019-01-19 01:36:35 -0800

    specs

--- src/MediBot.ts
+++ src/MediBot.ts
@@ -4,6 +4,10 @@ interface FBMessage extends Botkit.FacebookMessage {
   nlp: any;
 }
 
+const dataset2 = [
+  "Fuck you"
+];
+
 export function messageReceived(
   bot: Botkit.FacebookBot,
   msg: Botkit.FacebookMessage
@@ -22,3 +26,15 @@ export function messageReceived(
 function getFirstEntity(nlp: any, name: string) {
   return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
 }
+
+/*
+Tech cares because charity
+Alexa for functionality
+GCP for hosting
+Hedera for micropayments to charity
+EarthHack for random environmental charities
+IoT for LED light up with each new donation
+UCSC Arts for LED pattern
+
+Missing: Microsoft Data Security & ProductOps
+*/
